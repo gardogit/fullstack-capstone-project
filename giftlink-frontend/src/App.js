d06d631f94d7459fd48fb17162fc2d352688addf
gardogit <edgardo.irm@gmail.com>
2024-08-31 12:44:59 -0400

    SearchPage routes and navigation links on Navbar

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './components/HomePage/HomePage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -22,6 +23,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/search" element={<SearchPage/>} />
         </Routes>
     </AuthProvider>
   );
--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -13,6 +13,9 @@ export default function Navbar() {
                     <li className="nav-item">
                         <Link className="nav-link" to="/app">Gifts</Link>
                     </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/app/search">Search</Link>
+                    </li>
                 </ul>
             </div>
         </nav>
