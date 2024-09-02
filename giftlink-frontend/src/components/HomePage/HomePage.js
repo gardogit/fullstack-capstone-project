3857eda6d467d6159e5c555f208a0707e8d9ef6c
gardogit <edgardo.irm@gmail.com>
2024-09-02 19:50:52 -0400

    HomePage corrections on useNavigate to /app

--- giftlink-frontend/src/components/HomePage/HomePage.js
+++ giftlink-frontend/src/components/HomePage/HomePage.js
@@ -1,17 +1,22 @@
-// front/src/components/HomePage/HomePage.js
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 function HomePage() {
+    const navigate = useNavigate();
+  
+    const handleGetStartedClick = () => {
+      navigate('/app');
+    };
+  
     return (
-        <div className="container my-5 text-center">
-            <h1 className="display-4 mb-3">GiftLink</h1>
-            <h2 className="mb-4">Share Gifts and Joy!</h2>
-            <p className="lead">
-                "Sharing is the essence of community. It is through giving that we enrich and perpetuate both our lives and the lives of others."
-            </p>
-            <a href="/app" className="btn btn-primary">Get Started</a>
-        </div>
+      <div className="container my-5 text-center">
+        <h1 className="display-4 mb-3">GiftLink</h1>
+        <h2 className="mb-4">Share Gifts and Joy!</h2>
+        <p className="lead">
+          "Sharing is the essence of community. It is through giving that we enrich and perpetuate both our lives and the lives of others."
+        </p>
+        <button className="btn btn-primary" onClick={handleGetStartedClick}>Get Started</button>
+      </div>
     );
-}
+  }
 
 export default HomePage;
